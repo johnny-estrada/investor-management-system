969c43a34a704d97cac101b40b2a204421902ede
Johnny Estrada <johnnye562@yahoo.com>
2024-02-24 16:39:16 -0800

    Add Security functionality and variables

--- src/main/java/com/wellsfargo/counselor/entity/Security.java
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -1,5 +1,75 @@
 package com.wellsfargo.counselor.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Security {
-    
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private Date purchaseDate;
+
+    @Column(nullable = false)
+    private Float purchasePrice;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    public Security(String name, String category, Date purchaseDate, Float purchasePrice, Integer quantity) {
+        this.name = name;
+        this.category = category;
+        this.purchaseDate = purchaseDate;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory() {
+        this.category = category;
+    }
+
+    public Date purchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate() {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice() {
+        this.purchasePrice = purchasePrice;
+    }
 }
