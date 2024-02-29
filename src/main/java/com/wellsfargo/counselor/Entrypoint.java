e01fc9f7e578ceecb9031666bc51690e1f97d2ce
Johnny Estrada <johnnye562@yahoo.com>
2024-02-28 23:16:46 -0800

    Add bean to Entrypoint

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -1,14 +1,39 @@
 package com.wellsfargo.counselor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import com.wellsfargo.counselor.entity.Advisor;
+import com.wellsfargo.counselor.repository.AdvisorRepository;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Entrypoint {
+
+    private static final Logger log = LoggerFactory.getLogger(Entrypoint.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Entrypoint.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(AdvisorRepository repository) {
+        return args -> {
+            // save a few advisors
+            repository.save(new Advisor("Jack", "Bauer", "123 fir st", "555-555-5555", "jack@email"));
+            repository.save(new Advisor("Jack", "Bauer", "123 fir st", "555-555-5555", "jack@email"));
+            repository.save(new Advisor("Jack", "Bauer", "123 fir st", "555-555-5555", "jack@email"));
+            repository.save(new Advisor("Jack", "Bauer", "123 fir st", "555-555-5555", "jack@email"));
+
+            // fetch all customers
+            log.info("Advisor found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().forEach(advisor -> log.info(advisor.toString()));
+            log.info("");
+        };
+
+    }
 }
